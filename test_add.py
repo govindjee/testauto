9b263ff5a7dc787f24c6730a29ce6e023d272bda
govindjee <49498068+govindjee@users.noreply.github.com>
2022-04-03 23:36:58 +0530

    Add files via upload

--- test_add.py
+++ test_add.py
@@ -1,4 +1,13 @@
 import pytest
 from assign import add
+from weaterapi import get_wather
+
 def test_auto():
     assert 5==add(2,3)
+
+def test_add():
+    assert 3==add(1,2)
+
+
+def test_weater():
+    assert get_wather()==58
